a6f1f1ea7524990b96b0c6e629ca02a7d752fef9
Carlos dos Santos <cds@cds-software.com.br>
2020-06-29 23:54:36 -0300

    Novo Post

--- publica.cmd
+++ publica.cmd
@@ -2,7 +2,7 @@ git add .
 git commit -m "Novo Post"
 git push
 
-hugo -d ..\carloscds.github.io
+hugo -b https://carloscds.net/ -d ..\carloscds.github.io
 cd ..\carloscds.github.io
 git add .
 git commit -m "Novo Post"
